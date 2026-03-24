0e6cc00ed0141b21d26ec1e58860625d6b86ca38
jarek3660-cmd <jarek3660@gmail.com>
2026-03-23 20:00:35 -0600

    Create D_Flip_Flop_CLR.vhdl

---
+++ D_Flip_Flop_CLR.vhdl
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/12/2026 02:29:42 PM
+-- Design Name: 
+-- Module Name: D_Flip_Flop - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity D_Flip_Flop is
+    Port ( D,CLK,CLR : in STD_LOGIC;
+           Q : out STD_LOGIC);
+end D_Flip_Flop;
+
+architecture Behavioral of D_Flip_Flop is
+
+begin
+    state: process(CLK)
+    begin
+    if CLK'event      
+         if CLK = '0' then       
+            Q <= D;
+         end if;
+    end if;
+    end process;
+end Behavioral;
