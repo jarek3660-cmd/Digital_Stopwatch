7d74139eee1158e79737c70ca2fb2c7685632df6
jarek3660-cmd <jarek3660@gmail.com>
2026-03-26 15:09:56 -0600

    Update Stopwatch_Top_Module.vhdl

--- Stopwatch_Top_Module.vhdl
+++ Stopwatch_Top_Module.vhdl
@@ -1,63 +1,49 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/23/2026 07:24:39 PM
--- Design Name: 
--- Module Name: Four_Bit_Counter_Modularized - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity Stopwatch_Top_Module is
+    Port (
+        Clk_System  : in  STD_LOGIC;
+        Reset       : in  STD_LOGIC;
+        Cathode_7SD_Loc : out STD_LOGIC_VECTOR(7 downto 0);
+        Anode_7SD_Loc   : out STD_LOGIC_VECTOR(7 downto 0)
+    );
+end Stopwatch_Top_Module;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+architecture Behavioral of Stopwatch_Top_Module is
 
-entity Four_Bit_Counter_Modularized is
-    Port ( Clk_Sys : in STD_LOGIC;
-           Reset : in STD_LOGIC;
-           Count : out STD_LOGIC_VECTOR (3 downto 0));
-end Four_Bit_Counter_Modularized;
-
-architecture Behavioral of Four_Bit_Counter_Modularized is
-    component Frequency_Divider is
-        Port ( Clock_System : in STD_LOGIC;
-               Clock_1Hz : out STD_LOGIC);
+    component Four_Bit_Counter_Modularized is
+        Port (
+            Clk_Sys : in  STD_LOGIC;
+            Reset   : in  STD_LOGIC;
+            Count   : out STD_LOGIC_VECTOR(3 downto 0)
+        );
     end component;
-    
-    component D_Flip_Flop is
-        Port ( D,CLK, CLR : in STD_LOGIC;
-           Q : out STD_LOGIC);
+
+    component Digital_Display is
+        Port (
+            Count       : in  STD_LOGIC_VECTOR(3 downto 0);
+            Cathode_7SD : out STD_LOGIC_VECTOR(7 downto 0);
+            Anode_7SD   : out STD_LOGIC_VECTOR(7 downto 0)
+        );
     end component;
-    
-    signal temp    : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
-    signal notTemp : STD_LOGIC_VECTOR(3 downto 0);
-    signal Clk_1Hz : STD_LOGIC := '0';
-    
+
+    signal Count_4_Bit_Local : STD_LOGIC_VECTOR(3 downto 0);
+
 begin
-    notTemp <= not(temp);
-    fd:  Frequency_Divider port map(Clock_System => Clk_Sys, Clock_1Hz => Clk_1Hz);
-    ff0: D_Flip_Flop port map(D => notTemp(0), CLK => Clk_1Hz, Q => temp(0), CLR => Reset);
-    ff1: D_Flip_Flop port map(D => notTemp(1), CLK => temp(0), Q => temp(1), CLR => Reset);
-    ff2: D_Flip_Flop port map(D => notTemp(2), CLK => temp(1), Q => temp(2), CLR => Reset);
-    ff3: D_Flip_Flop port map(D => notTemp(3), CLK => temp(2), Q => temp(3), CLR => Reset);
+
+    Counter_Inst : Four_Bit_Counter_Modularized
+        port map (
+            Clk_Sys => Clk_System,
+            Reset   => Reset,
+            Count   => Count_4_Bit_Local
+        );
+
+    Display_Inst : Digital_Display
+        port map (
+            Count       => Count_4_Bit_Local,
+            Cathode_7SD => Cathode_7SD_Loc,
+            Anode_7SD   => Anode_7SD_Loc
+        );
 
 end Behavioral;
