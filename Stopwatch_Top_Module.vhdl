5ef7a55ecfe7143b570eb1fc44ae53e4bae4fee4
jarek3660-cmd <jarek3660@gmail.com>
2026-03-26 10:45:24 -0600

    Update Stopwatch_Top_Module.vhdl

--- Stopwatch_Top_Module.vhdl
+++ Stopwatch_Top_Module.vhdl
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 03/26/2026 10:07:27 AM
+-- Create Date: 03/23/2026 07:24:39 PM
 -- Design Name: 
--- Module Name: Stopwatch_Top_Module - Behavioral
+-- Module Name: Four_Bit_Counter_Modularized - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -31,22 +31,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity Stopwatch_Top_Module is
-    Port ( Clk_System : in STD_LOGIC;
-           Reset : in STD_LOGIC;
-           Cathode_7SD, Anode_7SD : out STD_LOGIC_VECTOR (7 downto 0));
-end Stopwatch_Top_Module;
-
-component Four_Bit_Counter_Modularized is 
+entity Four_Bit_Counter_Modularized is
     Port ( Clk_Sys : in STD_LOGIC;
            Reset : in STD_LOGIC;
            Count : out STD_LOGIC_VECTOR (3 downto 0));
-end component;
+end Four_Bit_Counter_Modularized;
 
-
-architecture Behavioral of Stopwatch_Top_Module is
-
+architecture Behavioral of Four_Bit_Counter_Modularized is
+    component Frequency_Divider is
+        Port ( Clock_System : in STD_LOGIC;
+               Clock_1Hz : out STD_LOGIC);
+    end component;
+    
+    component D_Flip_Flop is
+        Port ( D,CLK, CLR : in STD_LOGIC;
+           Q : out STD_LOGIC);
+    end component;
+    
+    signal temp    : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
+    signal notTemp : STD_LOGIC_VECTOR(3 downto 0);
+    signal Clk_1Hz : STD_LOGIC := '0';
+    
 begin
-
+    notTemp <= not(temp);
+    fd:  Frequency_Divider port map(Clock_System => Clk_Sys, Clock_1Hz => Clk_1Hz);
+    ff0: D_Flip_Flop port map(D => notTemp(0), CLK => Clk_1Hz, Q => temp(0), CLR => Reset);
+    ff1: D_Flip_Flop port map(D => notTemp(1), CLK => temp(0), Q => temp(1), CLR => Reset);
+    ff2: D_Flip_Flop port map(D => notTemp(2), CLK => temp(1), Q => temp(2), CLR => Reset);
+    ff3: D_Flip_Flop port map(D => notTemp(3), CLK => temp(2), Q => temp(3), CLR => Reset);
 
 end Behavioral;
