bc0301b8fe5b904d47796d9520d45864c2ad85c4
jarek3660-cmd <jarek3660@gmail.com>
2026-03-27 12:52:56 -0600

    Update D_Flip_Flop_CLR.vhdl

--- D_Flip_Flop_CLR.vhdl
+++ D_Flip_Flop_CLR.vhdl
@@ -1,51 +1,19 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/12/2026 02:29:42 PM
--- Design Name: 
--- Module Name: D_Flip_Flop - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity D_Flip_Flop is
-    Port ( D,CLK,CLR : in STD_LOGIC;
+    Port ( D, CLK, CLR : in STD_LOGIC;
            Q : out STD_LOGIC);
 end D_Flip_Flop;
 
 architecture Behavioral of D_Flip_Flop is
 begin
-    state: process(CLK)
+    process(CLK, CLR)
     begin
-    if falling_edge(CLK) then
-         if CLR = '1' then
-             Q <= '0';    
-         else       
+        if CLR = '1' then
+            Q <= '0';
+        elsif rising_edge(CLK) then
             Q <= D;
-         end if;
-    end if;
+        end if;
     end process;
 end Behavioral;
