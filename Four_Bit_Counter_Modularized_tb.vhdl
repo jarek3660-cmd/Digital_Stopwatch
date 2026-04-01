bb952fafa9020fd4d6cd5dbf163d9c249c0f405b
jarek3660-cmd <jarek3660@gmail.com>
2026-04-01 00:31:21 -0600

    Update Four_Bit_Counter_Modularized_tb.vhdl

--- Four_Bit_Counter_Modularized_tb.vhdl
+++ Four_Bit_Counter_Modularized_tb.vhdl
@@ -45,13 +45,27 @@ end component;
 
 signal Clk_Sys_Loc : STD_LOGIC;      
 signal Reset_Loc : STD_LOGIC;
-
+signal Count_Loc : STD_LOGIC_VECTOR(3 downto 0);
+constant Clock_100Mhz_Period : time := 10ns;
 begin
 
     uut: Four_Bit_Counter_Modularized Port map(
         Clk_Sys => Clk_Sys_Loc,
-        Reset => Reset_Loc);
+        Reset => Reset_Loc,
+        Count => Count_loc);
     
+    Clk_process : process
+    begin
+        Clk_Sys_Loc <= '0'; wait for Clock_100Mhz_Period/2;
+        Clk_Sys_Loc <= '1'; wait for Clock_100Mhz_Period/2;
+    end process;
     
-    
+    stim_process : process
+    begin
+        Reset_Loc <= '1';
+        wait for 50ns;
+        Reset_Loc <= '0';
+        wait for 50ns;
+     end process;
+     
 end Behavioral;
